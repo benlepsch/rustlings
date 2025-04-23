6e2606cf150ecddc85a7dfe55f9ea7f31699960d
Ben Lepsch <blepsch@gmail.com>
2025-04-23 14:03:23 +0000

    strings! big fan of these

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -1,13 +1,18 @@
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string.
+    input.trim()
+    // type errors :(
+    // input.chars().filter(|c| !c.is_whitespace()).collect::<String>()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There are multiple ways to do this.
+    format!("{input} world!")
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons".
+    input.replace("cars", "balloons")
 }
 
 fn main() {
