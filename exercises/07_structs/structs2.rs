27379ac806f2de0c5325fcc1bddcf6cfaa658973
Ben Lepsch <blepsch@gmail.com>
2025-05-22 19:00:33 +0000

    error handling

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -34,9 +34,11 @@ mod tests {
         let order_template = create_order_template();
 
         // TODO: Create your own order using the update syntax and template above!
-        let mut your_order = create_order_template();
-        your_order.name = "Hacker in Rust".to_string();
-        your_order.count = 1;
+        let mut your_order = Order { // create_order_template();
+            name: String::from("Hacker in Rust"),
+            count: 1,
+            ..order_template
+        };
 
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
